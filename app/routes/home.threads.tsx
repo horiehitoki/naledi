babba1b491a66fc80878f3a6aafe29515795ba15
marukun712 <marukun530@gmail.com>
2024-11-21 13:22:54 +0900

    🩹 スレッド欄のエラーを修正

--- app/routes/home.threads.tsx
+++ app/routes/home.threads.tsx
@@ -1,9 +1,15 @@
+import { Reaction } from "@prisma/client";
 import { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { PostData } from "@types";
 import { Heart, Repeat, MessageCircle } from "lucide-react";
 import Post from "~/components/timeline/post";
-import { PostView } from "~/generated/api/types/app/bsky/feed/defs";
+import {
+  FeedViewPost,
+  PostView,
+} from "~/generated/api/types/app/bsky/feed/defs";
 import { getSessionAgent } from "~/utils/auth/session";
+import { prisma } from "~/utils/db/prisma";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const agent = await getSessionAgent(request);
@@ -15,15 +21,42 @@ export const loader: LoaderFunction = async ({ request }) => {
 
   const threads = await agent.getPostThread({ uri: uri });
 
-  return { threads: threads.data.thread, uri: uri };
+  const replies = threads.data.thread.replies as [];
+
+  const post = threads.data.thread.post as PostView;
+
+  const originalPostReaction: Reaction[] = await prisma.reaction.findMany({
+    where: {
+      uri: post.uri,
+    },
+  });
+
+  //返信をリアクションデータ付きで返す
+  const data: PostData[] = await Promise.all(
+    replies.map(async (post: FeedViewPost) => {
+      const reaction: Reaction[] = await prisma.reaction.findMany({
+        where: {
+          uri: post.post.uri,
+        },
+      });
+
+      return { post, reaction };
+    })
+  );
+
+  return { originalPostReaction, threads: threads.data.thread, data, uri };
 };
 
 export default function Threads() {
-  const { threads, uri } = useLoaderData<typeof loader>();
+  const { originalPostReaction, threads, data, uri } =
+    useLoaderData<typeof loader>();
 
   return (
     <div className="max-w-2xl mx-auto px-4 py-6">
-      <Post post={threads.post} />
+      <Post
+        data={{ post: { post: threads.post }, reaction: originalPostReaction }}
+        key={threads.post.cid}
+      />
 
       <div className="bg-gray-50 dark:bg-gray-800 rounded-lg p-4 my-6">
         <div className="flex justify-center space-x-8">
@@ -57,8 +90,8 @@ export default function Threads() {
             返信 ({threads.post.replyCount})
           </h2>
           <div className="space-y-8">
-            {threads.replies.map((thread: { post: PostView }) => (
-              <Post post={thread.post} key={thread.post.cid} />
+            {data.map((data: PostData) => (
+              <Post data={data} key={data.post.post.cid} />
             ))}
           </div>
         </>
