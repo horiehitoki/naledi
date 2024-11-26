c76b23c98dc594d671133828f46a674f50acf07c
marukun712 <marukun530@gmail.com>
2024-11-26 14:19:24 +0900

    🩹 テーマの設定を修正

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -11,10 +11,7 @@ const sessionStorage = createCookieSessionStorage({
     httpOnly: true,
     sameSite: "lax",
     secrets: ["s3cr3t"],
-    // Set domain and secure only if in production
-    ...(isProduction
-      ? { domain: "your-production-domain.com", secure: true }
-      : {}),
+    ...(isProduction ? { domain: "stellarbsky.vercel.app", secure: true } : {}),
   },
 });
 
