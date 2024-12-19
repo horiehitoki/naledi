46def2ec1faa1b8eba6204a0c68ec20ad265e973
marukun712 <marukun530@gmail.com>
2024-12-19 10:54:40 +0900

    🐛 レスポンシブの調整

--- app/components/layout/main.tsx
+++ app/components/layout/main.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
 import SideBar from "../ui/sidebar";
+import FooterMenu from "../ui/mobileNavbar";
 
 export default function Main({ children }: { children: ReactNode }) {
   return (
-    <div className="container mx-auto p-4 flex">
-      <SideBar />
-      <div className="space-y-8 w-1/2 mx-auto">{children}</div>
+    <div className="container mx-auto p-4 md:flex">
+      <div className="hidden md:block">
+        <SideBar />
+      </div>
+      <div className="block md:hidden">
+        <FooterMenu />
+      </div>
+      <div className="space-y-8 md:w-1/2 mx-auto">{children}</div>
     </div>
   );
 }
