c23cbeab3daf1516f35723deb988ee9a3fa2100d
marukun712 <marukun530@gmail.com>
2025-03-03 00:18:48 +0900

    🐛 マイグレーション履歴の修正

--- appview/prisma/migrations/20241231000618_add_reactions/migration.sql
+++ appview/prisma/migrations/20241231000618_add_reactions/migration.sql
@@ -1,9 +1,11 @@
 -- CreateTable
 CREATE TABLE "Reaction" (
-    "id" TEXT NOT NULL,
+    "rkey" TEXT NOT NULL,
+    "post_uri" TEXT NOT NULL,
+    "post_cid" TEXT NOT NULL,
     "record" TEXT NOT NULL,
     "authorDid" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Reaction_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Reaction_pkey" PRIMARY KEY ("rkey")
 );
