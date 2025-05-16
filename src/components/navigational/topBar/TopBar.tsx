05dbce6d6fe47e0352a76c3d283e9d53c3178b4f
Hitoki Horie <horie@gunjo.org>
2025-05-16 11:46:35 +0900

    Update TopBar.tsx

--- src/components/navigational/topBar/TopBar.tsx
+++ src/components/navigational/topBar/TopBar.tsx
@@ -2,7 +2,8 @@
 
 import Button from "@/components/actions/button/Button";
 import Image from "next/image";
-import Link from "next/link";
+
+export default function TopBar() {
 
   return (
     <div className="bg-skin-base border-skin-base sticky top-0 z-[60] flex items-center justify-between border-b px-3 py-2.5 transition-all ease-linear md:hidden">
